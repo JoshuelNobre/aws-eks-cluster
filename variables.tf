77982446d24319e881b7c7e91172b92c900b6bca
Joshuel Nobre <nobrej1999@gmail.com>
2025-01-13 01:44:35 -0300

    cluster eks

---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "project_name" {
+
+}
+
+variable "region" {
+
+}
+
+variable "k8s_version" {
+  type = string
+}
+
+variable "ssm_vpc" {
+  type = string
+}
+
+variable "ssm_public_subnets" {
+  type = list(string)
+}
+
+variable "ssm_private_subnets" {
+  type = list(string)
+}
+
+variable "ssm_pods_subnets" {
+  type = list(string)
+}
+
+variable "auto_scale_options" {
+  type = object({
+    min     = number
+    max     = number
+    desired = number
+  })
+}
+
+variable "nodes_instance_sizes" {
+  type = list(string)
+}
