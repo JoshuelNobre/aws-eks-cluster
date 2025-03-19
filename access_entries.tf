ea1bf49d47b86480c9f0bd07ecbe51c4f3c7e40b
Joshuel Nobre <nobrej1999@gmail.com>
2025-03-18 23:30:41 -0300

    cluster eks fargate

--- access_entries.tf
+++ access_entries.tf
@@ -4,8 +4,8 @@ resource "aws_eks_access_entry" "nodes" {
   type          = "EC2_LINUX"
 }
 
-resource "aws_eks_access_entry" "fargate" {
-  cluster_name  = aws_eks_cluster.main.id
-  principal_arn = aws_iam_role.eks_nodes_role.arn
-  type          = "FARGATE_LINUX"
-}
+# resource "aws_eks_access_entry" "fargate" {
+#   cluster_name  = aws_eks_cluster.main.id
+#   principal_arn = aws_iam_role.eks_nodes_role.arn
+#   type          = "FARGATE_LINUX"
+# }
--- fargate.tf
+++ fargate.tf
@@ -1,4 +1,17 @@
-resource "aws_eks_fargate_profile" "chip" {
+# resource "aws_eks_fargate_profile" "chip" {
+#   cluster_name = aws_eks_cluster.main.name
+#   fargate_profile_name = "chip"
+
+#   pod_execution_role_arn = aws_iam_role.fargate.arn
+
+#   subnet_ids = data.aws_ssm_parameter.pods_subnets[*].value
+
+#   selector {
+#     namespace = "chip"
+#   }
+# }
+
+resource "aws_eks_fargate_profile" "wildcard" {
   cluster_name = aws_eks_cluster.main.name
   fargate_profile_name = "chip"
 
@@ -7,6 +20,6 @@ resource "aws_eks_fargate_profile" "chip" {
   subnet_ids = data.aws_ssm_parameter.pods_subnets[*].value
 
   selector {
-    namespace = "chip"
+    namespace = "*"
   }
 }
